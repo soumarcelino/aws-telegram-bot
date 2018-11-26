722f90fa4b853616f824c68351726598c645609a
soumarcelino <marcelinomatiasdesouza@gmail.com>
2018-11-25 23:01:13 -0300

    Add deploy tools

---
+++ tools/Deploy.js
@@ -0,0 +1,34 @@
+const spawn = require('child_process').exec
+
+const settings = {
+    "invocationMessage" : "/deploy",
+    "allowedUsers" : ["soumarcelino"]
+}
+
+class init {
+    constructor(bot,text,id,username){
+        this.bot = bot
+        this.text = text
+        this.id = id
+        this.username = username        
+    }
+
+    run() {
+       const welcomeMessage = `
+        🤖 I can Deploy projects
+       `
+       this.bot.sendMessage(this.id,welcomeMessage)
+       const project = this.text.split("/deploy")[1].trim()
+       const cmd = `git clone ${project} ~/`
+       spawn(cmd, (err, stdout) => {
+            if (err) {
+                this.bot.sendMessage(this.id,`Error: ${err}`)
+            }
+            this.bot.sendMessage(this.id,stdout)
+        });
+       
+    }
+    
+}
+
+module.exports = {init,settings}
--- tools/index.js
+++ tools/index.js
@@ -1,13 +1,15 @@
 const Update = require('./Update')
 const Start = require('./Start')
 const Cmd = require('./Cmd')
+const Deploy = require('./Deploy')
 
 const Gateway = require('./Gateway')
 
 const gateway = new Gateway([
     Update,
     Start,
-    Cmd
+    Cmd,
+    Deploy
 ])
 
 module.exports = {gateway}
