eb58af4935ddf1c385b2f22a546b71409328dd70
soumarcelino <marcelinomatiasdesouza@gmail.com>
2018-11-25 23:49:47 -0300

    /Format start message

--- Modules/Start.js
+++ Modules/Start.js
@@ -15,13 +15,15 @@ class init {
 
     run() {
        const welcomeMessage = `
-        🤖 Hi, I'm a super bot, now i can run 
-        /start  - View this message
-        /update - Update the bot
-        /cmd [command]   - Run some no interactive command
-        /deploy [ssh addr] - Deploy git project
+        <h3>🤖 Hi, I'm a super bot, now i can run</h3>
+        <p>
+           <b>/start</b>  - <i>View this message</i>
+           <b>/update</b> - <i>Update the bot</i>
+           <b>/cmd [ command ]</b> - <i>Run some no interactive command</i>
+           <b>/deploy [ ssh addr ]</b> - <i>Deploy git project</i>
+        </p>
        `
-       this.bot.sendMessage(this.id,welcomeMessage)
+       this.bot.sendMessage(this.id,welcomeMessage,{ parse_mode: 'html' })
        
     }
     
