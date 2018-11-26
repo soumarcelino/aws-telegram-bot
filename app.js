24190bb13ee2b15abb65f74fd29b8e15ae39f2e6
soumarcelino <marcelinomatiasdesouza@gmail.com>
2018-11-25 23:23:02 -0300

    Rename modules for modules

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
-const Tools = require ('./tools')
+const Modules = require ('./Modules')
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
-const gateway = Tools.gateway;
+const gateway = Modules.gateway;
 gateway.setBot(bot)
 bot.on('message', (msg) => {
     const { text, chat : { id }, from : { username } } = msg;
